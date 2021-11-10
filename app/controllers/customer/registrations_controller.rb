308f8164ff97792260cd8e43881f9a49a418495b
“takato15” <gshawks21@gmail.com>
2021-11-10 15:03:00 +0000

    [Add]ログインページ修正

--- app/controllers/customer/registrations_controller.rb
+++ app/controllers/customer/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Customer::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +61,38 @@ class Customer::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+  if
+    @customer.update(customer_params)
+    flash[:notice] = "登録情報を更新しました"
+    redirect_to customers_path
+  else
+    render :edit
+  end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_cord, :address, :phone_number, :is_deleted, :email)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_cord, :address, :phone_number])
+  end
+
+  private
+
+  def after_sign_up_path_for(resource)
+    customers_path
+  end
+
 end
