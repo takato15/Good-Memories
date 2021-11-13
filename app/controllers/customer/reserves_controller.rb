6748c14a95dd02b6f27e47ed861c64ce69bf763c
“takato15” <gshawks21@gmail.com>
2021-11-13 17:55:46 +0000

    [Add]予約機能追加

--- app/controllers/customer/reserves_controller.rb
+++ app/controllers/customer/reserves_controller.rb
@@ -1,2 +1,28 @@
 class Customer::ReservesController < ApplicationController
+  
+  def index
+    @reserves = Reserve.all
+  end
+  
+  def new
+    @reserve = Reserve.new
+  end
+  
+  def create
+    @reserve = Reserve.new(reserve_params)
+    if @reserve.save
+      redirect_to root_path
+      # redirect_to confirm_reserves(@reserve.id)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def reserve_params
+    params.require(:reserve).permit(:count, :reserve_day, :billing, :payment, :name, :post_cord, :address, :plan_name )
+  end
+  
 end
+
