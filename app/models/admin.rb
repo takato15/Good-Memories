83b2e6059beb032f9dd3df5f2af70d5a5d4c7c5e
“takato15” <gshawks21@gmail.com>
2021-11-28 12:51:35 +0000

    [Fix]seed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,13 +4,6 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
-  validates :post_cord, presence: true, format: { with: /\A\d{7}\z/, message: "半角数字、ハイフンなしで入力して下さい" }
-  validates :address, presence: true
-  validates :phone_number, presence: true, numericality: {only_integer: true}
   validates :email, presence: true, uniqueness: true
 
 end
